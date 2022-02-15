00ed43b46e63fcac8747323d077b9eda0c15cb2f
liuyuming11 <liuyuming11@jd.com>
2022-02-15 20:21:39 +0800

    测试docker镜像

--- .idea/src/main/java/com/sydml/k8s/KuberController.java
+++ .idea/src/main/java/com/sydml/k8s/KuberController.java
@@ -3,6 +3,12 @@ package com.sydml.k8s;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.time.LocalDateTime;
+import java.util.Enumeration;
+
 /**
  * @author liuyuming
  * @date 2021-10-13 9:51:43
@@ -11,6 +17,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class KuberController {
     @GetMapping
     public String helloK8s(){
-        return "hello k8s";
+        return getLocalIp() + " hello k8s =====> " + LocalDateTime.now().toString();
+    }
+    public static String getLocalIp() {
+        try {
+            Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            InetAddress ip = null;
+            while (allNetInterfaces.hasMoreElements()){
+                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+                if("lo".equals(netInterface.getName())){
+                    continue;
+                }
+                Enumeration addresses = netInterface.getInetAddresses();
+                while (addresses.hasMoreElements())
+                {
+                    ip = (InetAddress) addresses.nextElement();
+                    if (ip != null && ip instanceof Inet4Address)
+                    {
+                        return ip.getHostAddress();
+                    }
+                }
+            }
+
+            return  null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }catch (Throwable e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
